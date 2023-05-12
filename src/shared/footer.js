97579714b12ff4393ef98c8a052fca8c3340db35
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-05-12 17:41:24 +0300

    create responsive footer

--- src/shared/footer.js
+++ src/shared/footer.js
@@ -1,7 +1,69 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
+import Logo from "./Logo";
+import NavBar from "../components/navbar";
+import SocialBar from "../components/socialbar";
 
 const FooterApp = () => {
-  return <p>footer</p>;
+  const navItems = [
+    "Home",
+    "Solutions",
+    "Cases",
+    "About Us",
+    "Blog",
+    "Contact Us",
+  ];
+  const socials = [
+    {
+      name: "facebook",
+      url: "https://www.facebook.com",
+    },
+    {
+      name: "twitter",
+      url: "https://www.twitter.com",
+    },
+    {
+      name: "youtube",
+      url: "https://www.youtube.com",
+    },
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com",
+    },
+    {
+      name: "pinterest",
+      url: "https://www.pinterest.com",
+    },
+  ];
+  return (
+    <Stack
+      direction="column"
+      alignItems="center"
+      mt={10}
+      spacing={4}
+      padding={15}
+    >
+      <Box>
+        <Logo />
+      </Box>
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={{ xs: 1, sm: 3, md: 4 }}
+        display={{ xs: "none", sm: "none", md: "block", lg: "block" }}
+      >
+        {navItems.map((item) => (
+          <NavBar color="#1E1E1E" underline="hover" key={item}>
+            {item}
+          </NavBar>
+        ))}
+      </Stack>
+      <SocialBar socials={socials} />
+      <Typography variant="overline" component='p' textAlign="center">
+        Copyrights ©2020 Thefirm. All Right Reserved
+      </Typography>
+    </Stack>
+  );
 };
 
 export default FooterApp;
