c60e6da09b8b6fc15fad54c17b54026103ea9740
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-05-17 23:14:09 +0300

    responsive reasons to choose us section in home page

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -2,28 +2,57 @@ import React, { useState } from "react";
 import HomePageHeroSection from "../../components/hompageherosection";
 import OurPartnersSection from "../../components/ourpartnerssection";
 import SolutionsSection from "../../components/homepagesolutionssection";
-import { Stack, Grid } from "@mui/material";
+import { Stack, Grid, Box, CardMedia } from "@mui/material";
 import { Container, Typography } from "@mui/material";
 import ButtonApp from "../../shared/button";
+import Branding from "../../assets/brandingImg.png";
+import BrandingIcon from "../../assets/branding.svg";
+import ReportingIcon from "../../assets/reportingicon.svg";
+import MarketSearchingIcon from "../../assets/marketresearchicon.svg";
 
 const HomePage = () => {
-  const [clicked, setClicked] = useState(false);
-
-  const handleButtonClick = () => {
-    setClicked(!clicked);
-  };
-
+  const [clicked, setClicked] = useState(0);
+  const reasons = [
+    {
+      name: "Market Research",
+      icon: MarketSearchingIcon,
+      image: Branding,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+    },
+    {
+      name: "Branding",
+      icon: BrandingIcon,
+      image: Branding,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+    },
+    {
+      name: "Reporting",
+      icon: ReportingIcon,
+      image: Branding,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+    },
+    {
+      name: "Data Analysis",
+      icon: BrandingIcon,
+      image: Branding,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+    },
+  ];
   return (
     <>
       <HomePageHeroSection />
       <OurPartnersSection />
       <SolutionsSection />
       <Container>
-        <Stack justifyContent="center" alignItems="center" spacing={3}>
+        <Stack justifyContent="center" alignItems="center" spacing={7}>
           <Stack
             justifyContent="center"
             alignItems="center"
-            spacing={3}
+            spacing={4}
             width={"64%"}
           >
             <Typography variant="h4" textAlign="center">
@@ -36,19 +65,55 @@ const HomePage = () => {
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries,
             </Typography>
-            <Stack spacing={2} direction="row">
-              {[1, 2, 3, 4].map((item, index) => (
+            <Stack
+              spacing={3}
+              direction={{ lg: "row", md: "column", sm: "column" }}
+            >
+              {reasons.map((item, index) => (
                 <ButtonApp
                   key={index}
-                  onClick={handleButtonClick}
-                  variant={clicked ? "outlined" : "contained"}
-                  setClicked={setClicked}
+                  variant={index === clicked ? "contained" : "outlined"}
+                  // disabled={index !== clicked}
+                  onClick={() => setClicked(index)}
                 >
-                  Market Research
+                  {item.name}
                 </ButtonApp>
               ))}
             </Stack>
           </Stack>
+          {clicked !== null && (
+            <Stack
+              width={"90%"}
+              spacing={1}
+              alignItems={{
+                lg: "flex-start",
+                md: "center",
+                sm: "center",
+                xs: "center",
+              }}
+              direction={{ lg: "row", md: "column", sm: "column" }}
+            >
+              <CardMedia
+                component="img"
+                sx={{ width: "60%" }}
+                src={reasons[clicked].image}
+                alt="Reason Image"
+              />
+              <img src={reasons[clicked].icon} />
+              <Stack
+                paddingTop={"1.5rem"}
+                direction="column"
+                spacing={5}
+                width={"70%"}
+              >
+                <Typography variant="h6"> {reasons[clicked].name}</Typography>
+                <Typography variant="body2">
+                  {reasons[clicked].description}
+                </Typography>
+                <ButtonApp variant="contained">Read More</ButtonApp>
+              </Stack>
+            </Stack>
+          )}
         </Stack>
       </Container>
     </>
--- src/shared/button.js
+++ src/shared/button.js
@@ -1,12 +1,9 @@
 import { Button } from "@mui/material";
 import React from "react";
 
-const ButtonApp = ({ children, color, disabled, size, variant  , clicked , setClicked}) => {
-  const handleButtonClick = () => {
-    setClicked(!clicked);
-  };
+const ButtonApp = ({ children, color, disabled, size, variant , onClick}) => {
   return (
-    <Button color={color} disabled={disabled} size={size} variant={variant} onClick={handleButtonClick}>
+    <Button  color={color} disabled={disabled} size={size} variant={variant} onClick={onClick}>
       {children}
     </Button>
   );
