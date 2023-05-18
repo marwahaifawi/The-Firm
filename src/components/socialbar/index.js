d4c362d7f3acee90d9e79a7407b5fee44e4c5a75
MarwaSalah252 <117007118+MarwaSalah252@users.noreply.github.com>
2023-05-18 12:30:04 +0300

    add our blog section to home page, remoe unwanted comments and files

--- src/components/socialbar/index.js
+++ src/components/socialbar/index.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { IconButton } from "@material-ui/core";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import PinterestIcon from '@mui/icons-material/Pinterest';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { Stack , Box } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Stack, Box } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   iconButton: {
-    color: 'black', // set default icon color
+    color: "black",
     "&:hover": {
-      color: 'primary', // change color when hovered over
+      color: "primary",
     },
   },
 }));
@@ -30,7 +30,7 @@ const SocialBar = ({ socials }) => {
   };
 
   return (
-    <Stack direction={{xs:"column",sm:'column' ,md:"row", lg:'row'}}>
+    <Stack direction={{ xs: "column", sm: "column", md: "row", lg: "row" }}>
       {socials.map(({ name, url }, index) => {
         let icon;
         switch (name) {
@@ -61,14 +61,9 @@ const SocialBar = ({ socials }) => {
           >
             {hoveredIndex === index ? (
               // use iconHover when this icon is hovered over
-              <Box color="primary.main">
-                {icon}
-              </Box>
+              <Box color="primary.main">{icon}</Box>
             ) : (
-              // use default icon when not hovered over
-              <Box>
-                {icon}
-              </Box>
+              <Box>{icon}</Box>
             )}
           </IconButton>
         );
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,21 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import HomePageHeroSection from "../../components/hompageherosection";
 import OurPartnersSection from "../../components/ourpartnerssection";
 import SolutionsSection from "../../components/homepagesolutionssection";
 import ReasonsToChooseUs from "../../components/homepagereasonstochooseussection";
 import OurBlogs from "../../components/homepageourblogssection";
 
-
 const HomePage = () => {
-
   return (
     <>
       <HomePageHeroSection />
       <OurPartnersSection />
       <SolutionsSection />
-      <ReasonsToChooseUs/>
-      <OurBlogs/>
-    
+      <ReasonsToChooseUs />
+      <OurBlogs />
     </>
   );
 };
