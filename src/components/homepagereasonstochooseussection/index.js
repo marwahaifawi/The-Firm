dd7b19bb0fc8645bf215e2071722bd1db1b6be0b
MarwaSalah252 <117007118+MarwaSalah252@users.noreply.github.com>
2023-05-18 09:19:31 +0300

    make a reasons to chooses us section in hompe page component, add real data to show

---
+++ src/components/homepagereasonstochooseussection/index.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { Stack, CardMedia } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import ButtonApp from "../../shared/button";
+import Branding from "../../assets/brandingImg.png";
+import BrandingIcon from "../../assets/branding.svg";
+import Reporting from "../../assets/reporting.png";
+import ReportingIcon from "../../assets/reportingicon.svg";
+import MarketResearch from "../../assets/marhetresearch.png";
+import MarketSearchingIcon from "../../assets/marketresearchicon.svg";
+import DataAnalysis from "../../assets/dataanalysis.png";
+import DataAnalysisIcon from "../../assets/dataanalysisIcon.svg"
+
+const ReasonsToChooseUs = () => {
+  const [clicked, setClicked] = useState(0);
+  const reasons = [
+    {
+      name: "Market Research",
+      icon: MarketSearchingIcon,
+      image: MarketResearch,
+      description:
+        "Market research helps you gain valuable insights into your target audience's preferences, needs, and behaviors. By conducting research, you can identify your target market's demographics, interests, and purchasing habits, enabling you to tailor your products or services to meet their specific demands.",
+    },
+    {
+      name: "Branding",
+      icon: BrandingIcon,
+      image: Branding,
+      description:
+        "Our firm specializes in creating professional and visually appealing websites that align with your brand identity. We understand the importance of a strong brand presence and design websites that reflect your company's values, mission, and unique selling points.",
+    },
+    {
+      name: "Reporting",
+      icon: ReportingIcon,
+      image: Reporting,
+      description:
+        "Reports enable businesses to track their progress towards set goals and objectives. By comparing actual performance against targets, reports highlight areas where the business is excelling and areas that require improvement.",
+    },
+    {
+      name: "Data Analysis",
+      icon: DataAnalysisIcon,
+      image: DataAnalysis,
+      description:
+        "Data analysis provides valuable information that helps businesses make informed decisions. By analyzing data patterns, trends, and correlations, organizations can identify opportunities, anticipate market shifts, and make strategic choices to optimize their operations and achieve their goals.",
+    },
+  ];
+
+  return (
+    <Container>
+      <Stack justifyContent="center" alignItems="center" spacing={7}>
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          spacing={4}
+          width={"64%"}
+        >
+          <Typography variant="h4" textAlign="center">
+            Reason to Choose Us
+          </Typography>
+          <Typography textAlign="center" variant="OVERLINE TEXT">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries,
+          </Typography>
+          <Stack
+            spacing={3}
+            direction={{ lg: "row", md: "column", sm: "column" }}
+          >
+            {reasons.map((item, index) => (
+              <ButtonApp
+                key={index}
+                variant={index === clicked ? "contained" : "outlined"}
+                onClick={() => setClicked(index)}
+              >
+                {item.name}
+              </ButtonApp>
+            ))}
+          </Stack>
+        </Stack>
+        {clicked !== null && (
+          <Stack
+            width={"90%"}
+            spacing={1}
+            alignItems={{
+              lg: "flex-start",
+              md: "center",
+              sm: "center",
+              xs: "center",
+            }}
+            direction={{ lg: "row", md: "column", sm: "column" }}
+          >
+            <CardMedia
+              component="img"
+              sx={{ width: "60%" }}
+              src={reasons[clicked].image}
+              alt="Reason Image"
+            />
+            <img src={reasons[clicked].icon} alt="reasonImage" />
+            <Stack
+              paddingTop={"1.5rem"}
+              direction="column"
+              spacing={5}
+              width={"70%"}
+            >
+              <Typography variant="h6"> {reasons[clicked].name}</Typography>
+              <Typography variant="body2">
+                {reasons[clicked].description}
+              </Typography>
+              <ButtonApp variant="contained">Read More</ButtonApp>
+            </Stack>
+          </Stack>
+        )}
+      </Stack>
+    </Container>
+  );
+};
+
+export default ReasonsToChooseUs;
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -2,120 +2,18 @@ import React, { useState } from "react";
 import HomePageHeroSection from "../../components/hompageherosection";
 import OurPartnersSection from "../../components/ourpartnerssection";
 import SolutionsSection from "../../components/homepagesolutionssection";
-import { Stack, Grid, Box, CardMedia } from "@mui/material";
-import { Container, Typography } from "@mui/material";
-import ButtonApp from "../../shared/button";
-import Branding from "../../assets/brandingImg.png";
-import BrandingIcon from "../../assets/branding.svg";
-import ReportingIcon from "../../assets/reportingicon.svg";
-import MarketSearchingIcon from "../../assets/marketresearchicon.svg";
+import ReasonsToChooseUs from "../../components/homepagereasonstochooseussection";
+
 
 const HomePage = () => {
-  const [clicked, setClicked] = useState(0);
-  const reasons = [
-    {
-      name: "Market Research",
-      icon: MarketSearchingIcon,
-      image: Branding,
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
-    },
-    {
-      name: "Branding",
-      icon: BrandingIcon,
-      image: Branding,
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
-    },
-    {
-      name: "Reporting",
-      icon: ReportingIcon,
-      image: Branding,
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
-    },
-    {
-      name: "Data Analysis",
-      icon: BrandingIcon,
-      image: Branding,
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
-    },
-  ];
+
   return (
     <>
       <HomePageHeroSection />
       <OurPartnersSection />
       <SolutionsSection />
-      <Container>
-        <Stack justifyContent="center" alignItems="center" spacing={7}>
-          <Stack
-            justifyContent="center"
-            alignItems="center"
-            spacing={4}
-            width={"64%"}
-          >
-            <Typography variant="h4" textAlign="center">
-              Reason to Choose Us
-            </Typography>
-            <Typography textAlign="center" variant="OVERLINE TEXT">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries,
-            </Typography>
-            <Stack
-              spacing={3}
-              direction={{ lg: "row", md: "column", sm: "column" }}
-            >
-              {reasons.map((item, index) => (
-                <ButtonApp
-                  key={index}
-                  variant={index === clicked ? "contained" : "outlined"}
-                  // disabled={index !== clicked}
-                  onClick={() => setClicked(index)}
-                >
-                  {item.name}
-                </ButtonApp>
-              ))}
-            </Stack>
-          </Stack>
-          {clicked !== null && (
-            <Stack
-              width={"90%"}
-              spacing={1}
-              alignItems={{
-                lg: "flex-start",
-                md: "center",
-                sm: "center",
-                xs: "center",
-              }}
-              direction={{ lg: "row", md: "column", sm: "column" }}
-            >
-              <CardMedia
-                component="img"
-                sx={{ width: "60%" }}
-                src={reasons[clicked].image}
-                alt="Reason Image"
-              />
-              <img src={reasons[clicked].icon} />
-              <Stack
-                paddingTop={"1.5rem"}
-                direction="column"
-                spacing={5}
-                width={"70%"}
-              >
-                <Typography variant="h6"> {reasons[clicked].name}</Typography>
-                <Typography variant="body2">
-                  {reasons[clicked].description}
-                </Typography>
-                <ButtonApp variant="contained">Read More</ButtonApp>
-              </Stack>
-            </Stack>
-          )}
-        </Stack>
-      </Container>
+      <ReasonsToChooseUs/>
+    
     </>
   );
 };
