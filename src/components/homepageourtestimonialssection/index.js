129b279da3c6504f5fb9bd1beabb1f1510e18349
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-05-20 01:39:58 +0300

    add out testimonials swiper to home page

--- src/components/homepageourtestimonialssection/index.js
+++ src/components/homepageourtestimonialssection/index.js
@@ -1,16 +1,15 @@
 import { CardMedia, Container, Stack, Typography } from "@mui/material";
 import React from "react";
 import Testimonial1 from "../../assets/testimonial1.png";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import Testimonial2 from "../../assets/testimonial2.png";
+import Testimonial3 from "../../assets/testimonial3.png";
 
-// Import Swiper styles
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
 import styles from "./index.module.css";
 
-// import required modules
 import { Pagination } from "swiper";
 const testimonials = [
   {
@@ -19,12 +18,22 @@ const testimonials = [
       "Dive into the world of Learning Management Systems (LMS) and discover how they revolutionize education. Explore the benefits, features, and implementation of LMS .",
   },
   {
-    image: Testimonial1,
+    image: Testimonial2,
     description:
       "Dive into the world of Learning Management Systems (LMS) and discover how they revolutionize education. Explore the benefits, features, and implementation of LMS .",
   },
   {
     image: Testimonial1,
+    description:
+      "Dive into the world of Learning Management Systems (LMS) and discover how they revolutionize education. Explore the benefits, features, and implementation of LMS .",
+  },
+  {
+    image: Testimonial3,
+    description:
+      "Dive into the world of Learning Management Systems (LMS) and discover how they revolutionize education. Explore the benefits, features, and implementation of LMS .",
+  },
+  {
+    image: Testimonial2,
     description:
       "Dive into the world of Learning Management Systems (LMS) and discover how they revolutionize education. Explore the benefits, features, and implementation of LMS .",
   },
@@ -42,24 +51,16 @@ const OurTestimonials = () => {
           clickable: true,
         }}
         modules={[Pagination]}
+        className={styles.swiper}
       >
-        <SwiperSlide>
-          <Stack alignItems="center" spacing={2}>
-            <CardMedia sx={{ height: 100, width: 100 }} image={Testimonial1} />
-            <Typography textAlign="center">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took type scrambled
-              it to make a type specimen book. It has survived not only five
-              centuries,
-            </Typography>
-          </Stack>
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
+        {testimonials.map((item, index) => (
+          <SwiperSlide key={index}>
+            <Stack alignItems="center" spacing={2}>
+              <CardMedia sx={{ height: 100, width: 100 }} image={item.image} />
+              <Typography textAlign="center">{item.description}</Typography>
+            </Stack>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </Container>
   );
