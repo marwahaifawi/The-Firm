63d23f9c52908b5c22f5589e0b52f7a6643bc564
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-05-22 22:00:02 +0300

    responsive get in touch section in home page

--- src/components/homepagegetintouch/index.js
+++ src/components/homepagegetintouch/index.js
@@ -2,97 +2,85 @@ import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
-import { Stack } from "@mui/material";
+import { Container, Stack } from "@mui/material";
 import ButtonApp from "../../shared/button";
 
 const subjects = [
   {
-    value: "USD",
-    label: "$",
+    value: "Cases",
+    label: "Cases",
   },
   {
-    value: "EUR",
-    label: "€",
+    value: "Blogs",
+    label: "Blogs",
   },
   {
-    value: "BTC",
-    label: "฿",
+    value: "Solutions",
+    label: "Solutions",
   },
   {
-    value: "JPY",
-    label: "¥",
+    value: "Appointments",
+    label: "Appointments",
   },
 ];
 
 const GetInTouch = () => {
   return (
-    <Stack
-      alignItems="center"
-      justifyContent="center"
-      sx={{
-        height: "500px",
-        width: "100%",
-        backgroundImage: `url('../../assets/getInTouchImg.png')`,
-      }}
-    >
-      <Box
-        component="form"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column",
-          rowGap: "2rem",
-          justifyContent: "center",
-          borderRadius: "15px",
-          backgroundColor: "red",
-          height: "50%",
-          width: "50%",
-          padding: "10rem 1rem 5rem 1rem",
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
+    <Container><Box
+    component="form"
+    sx={{
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      rowGap: "3rem",
+      borderRadius: "15px",
+      boxShadow: "0px 19px 34px 14px rgba(0, 0, 0, 0.06)",
+      padding: "10rem 2rem 5rem 2rem",
+      "& .MuiTextField-root": { m: 2, width: "30ch" },
+    }}
+    noValidate
+    autoComplete="no"
+  >
+    <div>
+      <TextField
+        id="standard-required"
+        label="Name"
+        defaultValue="Your Name"
+        variant="standard"
+      />
+      <TextField
+        id="standard-required"
+        label="Email"
+        defaultValue="Your Email"
+        variant="standard"
+      />
+    </div>
+    <div>
+      <TextField
+        id="standard-required"
+        label="Phone"
+        defaultValue="05********"
+        variant="standard"
+      />
+      <TextField
+        id="standard-select-currency"
+        select
+        label="Subject"
+        defaultValue=""
+        variant="standard"
       >
-        <div>
-          <TextField
-            id="standard-required"
-            label="Name"
-            defaultValue="Your Name"
-            variant="standard"
-          />
-          <TextField
-            id="standard-required"
-            label="Email"
-            defaultValue="Your Email"
-            variant="standard"
-          />
-        </div>
-        <div>
-          <TextField
-            id="standard-required"
-            label="Phone"
-            defaultValue="05********"
-            variant="standard"
-          />
-          <TextField
-            id="standard-select-currency"
-            select
-            label="Select"
-            defaultValue=""
-            variant="standard"
-          >
-            {subjects.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-        </div>
-        <div>
-          <ButtonApp variant="contained">Submit</ButtonApp>
-        </div>
-      </Box>
-    </Stack>
+        {subjects.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+    </div>
+    <div>
+      <ButtonApp variant="contained">Submit</ButtonApp>
+    </div>
+  </Box></Container>
+    
   );
 };
 
