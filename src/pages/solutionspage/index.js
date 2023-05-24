b66d5b74174d9f40e4832a6a3f2bdacb1266159a
MarwaSalah252 <117007118+MarwaSalah252@users.noreply.github.com>
2023-05-24 11:36:08 +0300

    add solution description sections in solutions page

--- src/pages/solutionspage/index.js
+++ src/pages/solutionspage/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import SolutionsSection from "../../components/homepagesolutionssection";
 import { Container } from "@mui/material";
 import OurSolutionsCards from "../../components/solutionpageoursolutions";
+import SolutionDescription from "../../components/solutionpagesolutiondescriptionsection";
 
 const SolutionPage = () => {
   return (
     <Container>
       <SolutionsSection />
       <OurSolutionsCards/>
+      <SolutionDescription/>
+      
     </Container>
   );
 };
