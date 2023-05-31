acbb1e95756133984691097d9171399c30a30f84
MarwaSalah252 <117007118+MarwaSalah252@users.noreply.github.com>
2023-05-31 12:08:16 +0300

    add firbase project data and protect them localy by env.local file

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,8 @@
-
-
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBIj2GOoCIO7KyhUVpJXLoYjpHj_veSiHw",
-    authDomain: "thefirmauth.firebaseapp.com",
-    projectId: "thefirmauth",
-    storageBucket: "thefirmauth.appspot.com",
-    messagingSenderId: "416538992226",
-    appId: "1:416538992226:web:9c3917879f76476f75fb3b",
-    measurementId: "G-H1V5B9R82C"
-  };
+  apiKey: process.env.REACT_API_FIRBASE_API_KEY,
+  authDomain: process.env.REACT_API_FIRBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_API_FIRBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_API_FIRBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_API_FIRBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_API_FIRBASE_APP_ID,
+};
