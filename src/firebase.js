65ca63d476c163635223a4d40b5ba5e5d096bdf8
MarwaSalah252 <117007118+MarwaSalah252@users.noreply.github.com>
2023-05-31 11:41:53 +0300

    add firebase file

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIj2GOoCIO7KyhUVpJXLoYjpHj_veSiHw",
+    authDomain: "thefirmauth.firebaseapp.com",
+    projectId: "thefirmauth",
+    storageBucket: "thefirmauth.appspot.com",
+    messagingSenderId: "416538992226",
+    appId: "1:416538992226:web:9c3917879f76476f75fb3b",
+    measurementId: "G-H1V5B9R82C"
+  };
