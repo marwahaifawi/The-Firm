6268fbfaa694193cf4dcc0b3a0d5021858900d2e
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-06-11 02:18:29 +0300

    separate solution data into file

--- src/components/solutionpagesolutionsdescriptions/index.js
+++ src/components/solutionpagesolutionsdescriptions/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Stack, Typography } from "@mui/material";
+
 const SolutionsDescriptions = ({ solution, description, image, direction }) => {
   return (
     <Stack
       direction={{ lg: direction, md: direction, sm: "column", xs: "column" }}
-      alignItems={"center"}
-      justifyContent={"space-evenly"}
-      p={"1rem"}
+      alignItems="center"
+      justifyContent="space-evenly"
+      p="1rem"
       spacing={5}
     >
-      <Stack width={"100%"} p={"1rem"}>
-        <Typography  textAlign={"center"} gutterBottom variant="h5">
+      <Stack width="100%" p="1rem">
+        <Typography textAlign="center" gutterBottom variant="h5">
           {solution}
         </Typography>
-        <Typography textAlign={"center"} variant="body2" lineHeight={2.5}>
+        <Typography textAlign="center" variant="body2" lineHeight={2.5}>
           {description}
         </Typography>
       </Stack>
