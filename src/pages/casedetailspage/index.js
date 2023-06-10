700a4a98e41ad813c36dfc3ff69f34e381e07e11
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-06-11 01:51:50 +0300

    Added a loading state variable to show a loading indicator while cases are being fetched from the API. Wrapped the API call in a try-catch block to handle any errors that may occur during the request. Added a loading indicator to display Loading... while the cases are being fetched.Handled errors  in case of unsuccessful API requests and logged the error to the console. Used finally to set the loading state to false after the API request is completed. Adjusted the indentation and added consistent quotation marks for attribute values.

--- src/pages/casedetailspage/index.js
+++ src/pages/casedetailspage/index.js
@@ -6,28 +6,39 @@ import api from "../../api/api";
 
 const CaseDetailsPage = () => {
   const [cases, setCases] = useState([]);
-  const getCases = async () => {
-    const response = await api.get("/cases");
-    return response.data;
-  };
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const { caseID } = useParams();
 
   useEffect(() => {
-    const getAllCases = async () => {
-      const allCases = await getCases();
-      if (allCases) setCases(allCases);
+    const getCases = async () => {
+      try {
+        const response = await api.get("/cases");
+        setCases(response.data);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
     };
-    getAllCases();
+
+    getCases();
   }, []);
 
-  const { caseID } = useParams();
+  if (loading) {
+    // Show a loading indicator while fetching cases
+    return <Typography variant="body2" align="center">Loading...</Typography>;
+  }
+
+  if (error) {
+    // Show an error message if there was an error fetching cases
+    return <Typography variant="body2" align="center">An error occurred while fetching the case.</Typography>;
+  }
+
   const singleCase = cases.find((caseObj) => caseObj.id === parseInt(caseID));
   if (!singleCase) {
     // Case not found, handle this case (e.g., show a message or redirect)
-    return (
-      <Typography variant="body2" align="center">
-        Case not found.
-      </Typography>
-    );
+    return <Typography variant="body2" align="center">Case not found.</Typography>;
   }
 
   return (
@@ -38,10 +49,10 @@ const CaseDetailsPage = () => {
         image={singleCase.image}
       />
       <Stack p={2} spacing={1} mt={10}>
-        <Typography textAlign={"center"} gutterBottom variant="h4">
+        <Typography textAlign="center" gutterBottom variant="h4">
           {singleCase.title}
         </Typography>
-        <Typography textAlign={"center"} variant="body2" lineHeight={2}>
+        <Typography textAlign="center" variant="body2" lineHeight={2}>
           {singleCase.details}
         </Typography>
       </Stack>
